8035c1d8f4acafa37805f0f3b1fc82bc22fa635c
Jazmin Esqueda <jazminesqueda@csu.fullerton.edu>
2020-09-16 21:17:50 -0700

    Adding L/R buttons

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
 var HIDDEN_DETAIL_CLASS = 'hidden-detail';
 var TINY_EFFECT_CLASS = 'is-tiny';
 var ESC_KEY = 27;
+var lrButtons='[data-image-button="button"]';
 
 
 function setDetails(imageUrl, titleText) {  'use strict';
@@ -69,11 +70,73 @@ function addKeyPressHandler() {
     });
 }
 
+//---------------------button stuff---------------
+
+function addButtonHandler() {
+    "use strict";
+    var getCurrTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
+    var buttons = document.querySelectorAll(lrButtons);
+    var bArr = [].slice.call(buttons);
+    var lButton = bArr[0];
+    var rButton = bArr[1];
+    var thumbsArr = getThumbnailsArray();
+    var currImage;
+    var currTitle;
+  
+    //left button
+    lButton.addEventListener("click", function(event) {
+      event.preventDefault();
+  
+      for (var i = 0; i < thumbsArr.length; i++) {
+        if (thumbsArr[i].getAttribute("data-image-title") == getCurrTitle.textContent) {
+          if (i == 0) {
+            currImage = imageFromThumb(thumbsArr[thumbsArr.length - 1]);
+            currTitle = titleFromThumb(thumbsArr[thumbsArr.length - 1]);
+            setDetails(currImage, currTitle);
+            break;
+          } else if (i != 0) {
+            currImage = imageFromThumb(thumbsArr[i - 1]);
+            currTitle = titleFromThumb(thumbsArr[i - 1]);
+            setDetails(currImage, currTitle);
+          }
+        }
+      }
+      showDetails();
+  
+    });
+  
+    //right button
+    rButton.addEventListener("click", function(event) {
+      event.preventDefault();
+  
+      for (var i = 0; i < thumbsArr.length; i++) {
+        if (thumbsArr[i].getAttribute("data-image-title") == getCurrTitle.textContent) {
+          if (i == thumbsArr.length - 1) {
+            currImage = imageFromThumb(thumbsArr[0]);
+            currTitle = titleFromThumb(thumbsArr[0]);
+            setDetails(currImage, currTitle);
+          } else {
+            currImage = imageFromThumb(thumbsArr[i + 1]);
+            currTitle = titleFromThumb(thumbsArr[i + 1]);
+            setDetails(currImage, currTitle);
+            break;
+          }
+        }
+      }
+      showDetails();
+    });
+  }
+
+
+//---------------end of button stuff--------------
+
 function initializeEvents() {
     'use strict';
     var thumbnails = getThumbnailsArray();
     thumbnails.forEach(addThumbClickHandler);
     addKeyPressHandler();
+    addButtonHandler();
+
 }
 
 initializeEvents();
